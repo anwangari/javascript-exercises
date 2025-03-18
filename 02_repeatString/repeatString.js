c6b71fb2fedd09237a97dbda831d09d45f271c27
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-03-19 00:04:50 +0300

    Modified:   repeatString.js Modified:   repeatString.spec.js
    Completed the algorithm to repeat a string several times using a while loop,
    and removed unskipped all the tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numberOfTimesToRepeatString) {
+    if (numberOfTimesToRepeatString < 0) {
+        return "ERROR";
+    }
 
+    let arr = [];
+    let i = 0;
+    while (i < numberOfTimesToRepeatString) {
+        arr.push(stringToRepeat);
+        i++;
+    }
+    let repeatedStrings = arr.toString();
+
+    repeatedStrings = repeatedStrings.replaceAll(",", "");
+
+    return repeatedStrings;
 };
 
 // Do not edit below this line
