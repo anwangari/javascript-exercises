116a972eccec21039bb2a32ecf8568c4f8cae115
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-03-24 22:17:36 +0300

    Modified:   03_reverseString/reverseString.js Modified:   03_reverseString/reverseString.spec.js Modified:   04_removeFromArray/removeFromArray.js Modified:   04_removeFromArray/removeFromArray.spec.js
    Modified 'reverseString' and 'removeFromArray' directories to match their
    description and pass thier respective tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let reversedString = "";
+    let strLenth = inputString.length;
+    
+    while ((strLenth > 0)) {
+        reversedString += inputString[strLenth - 1]
+        strLenth--
+    };
+    return reversedString;
 };
+
+reverseString("Hello")
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    // find the element from an array and remove it
+    // ensure it is of the same type
+    // ensure function can accept several optional elements
+    let arr = array;
+    
+    // Finding the index of the element to remove
+    for (index in elementsToRemove) {
+        for (elementIndex in arr) {
+            let element = arr[elementIndex];
+            if ((element == elementsToRemove[index]) && typeof(element) == typeof(elementsToRemove[index])) {
+                // remove all elements from array
+                arr = arr.filter(item => item !== element)
+            };
+        };
+    };
+    
+    return arr
 };
+let result = removeFromArray([3, 2, 3, 3, 4], 4, 3, 5)
 
 // Do not edit below this line
 module.exports = removeFromArray;
