695856e6884a9972225c58d213d8e882751ec145
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-03-27 17:48:25 +0300

    Modified:   tempConversion.js Modified:   tempConversion.spec.js
    Added formular to convert temperature and return 1 decimal place in `tempConversion.js`

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let celsius = ((F - 32) * (5 / 9))
+  celsius = Math.round((celsius * 10).toFixed(1)) / 10
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let far = (C * (9 / 5) + 32)
+  far = Math.round((far * 10).toFixed(1)) / 10
+  return far
 };
 
 // Do not edit below this line
