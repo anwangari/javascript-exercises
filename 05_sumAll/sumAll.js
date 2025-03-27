015763a7e74a574ee58735349762d70de9220a85
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-03-27 15:40:27 +0300

    Modified:   sumAll.js Modified:   sumAll.spec.js
    Added to the function  `sumAll`, logic to sum all the number within a given range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let largerNumber = secondNumber;
 
+    function sum(number1, number2) {
+        let total = 0;
+        for (i = number1; i < (number2 + 1); i++) {
+            total += i;
+        };
+        console.log(total);
+        return total;
+    };
+
+    if (
+        ((firstNumber > 0 && largerNumber > 0)) &&
+        (Number.isInteger(firstNumber) && Number.isInteger(largerNumber))
+    ) {
+        if (firstNumber > largerNumber) {
+            largerNumber = firstNumber;
+            firstNumber = secondNumber;
+        };
+    
+        let totalSum = sum(firstNumber, largerNumber)
+        return totalSum
+
+    } else {
+        return "ERROR"
+    };
 };
+
+sumAll(1, 5)
 
 // Do not edit below this line
 module.exports = sumAll;
