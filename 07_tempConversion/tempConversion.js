1be13ea87611488aaee9a58e39e60478f2f48959
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-03-27 17:56:29 +0300

    modified:   tempConversion.js
    Modified logic code to look less bulky

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,9 @@
 const convertToCelsius = function(F) {
-  let celsius = ((F - 32) * (5 / 9))
-  celsius = Math.round((celsius * 10).toFixed(1)) / 10
-  return celsius
+  return Math.round((F - 32) * (5 / 9) * 10) / 10
 };
 
 const convertToFahrenheit = function(C) {
-  let far = (C * (9 / 5) + 32)
-  far = Math.round((far * 10).toFixed(1)) / 10
-  return far
+  return Math.round((C * (9 / 5) + 32) * 10) / 10
 };
 
 // Do not edit below this line
