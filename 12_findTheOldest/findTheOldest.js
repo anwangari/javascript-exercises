a38aea10766dcda821aa1748cc82164e0922a78e
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-06-03 17:57:34 +0300

    modified:   findTheOldest.js modified:   findTheOldest.spec.js
    Added logic to find the oldest person given an array of persons with their
    birth years and death years (including those still alive)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
+    let oldest = 0;
+    let k = 0;
 
+    for (let i = k; i < array.length; i++) {
+        if (array[i].yearOfDeath === undefined) {
+            array[i].yearOfDeath = currentYear;
+          }
+
+        let age = array[i].yearOfDeath - array[i].yearOfBirth;
+        if (age > oldest) {
+            oldest = age;
+            k = i;
+        }
+    }
+
+    return array[k]
 };
 
 // Do not edit below this line
